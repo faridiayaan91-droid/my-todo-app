3915f44489b5b139b77e120e801dd36763a25cb4
Ayan <faridiayaan91@gmail.com>
2026-08-26 18:20:57 +0530

    fix application context

--- run.py
+++ run.py
@@ -2,14 +2,15 @@ from app import create_app, db
 from app.models import User, Task
 
 application = create_app()
-with app.app_context():
+
+with application.app_context():
     db.create_all()
-    # Default admin auto-create
-    if not User.query.filter_by(username='admin').first():
-        admin = User(username='admin', password='1234')
+
+    if not User.query.filter_by(username="admin").first():
+        admin = User(username="admin", password="1234")
         db.session.add(admin)
         db.session.commit()
-        print("✅ Default admin created!")
+        print("Default admin created!")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    application.run(debug=True, host="0.0.0.0", port=5000)
