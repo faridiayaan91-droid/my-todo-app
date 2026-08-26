fbe3126c886150a4feed1365badf6da023238359
Ayan <faridiayaan91@gmail.com>
2026-08-26 18:09:54 +0530

    fix render app

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import create_app, db
 from app.models import User, Task
 
-app = create_app()
+application = create_app()
 with app.app_context():
     db.create_all()
     # Default admin auto-create
